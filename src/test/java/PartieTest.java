5efdeb8b8ab50e7bc95ddff3ea3e339822d9f831
B-Mahdj <bilal.mahdjoubi95@gmail.com>
2022-05-05 19:40:49 +0200

    Starting first classes

---
+++ src/test/java/PartieTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+public class PartieTest {
+
+    @Test
+    public void testCreatePartie(){
+        Partie partie = new Partie();
+    }
+
+    @Test
+    public void testCreatePartie
+}
